f520d2e1b4d610e254737bd12a5f366bdb569286
SulRang <37802354+SulRang@users.noreply.github.com>
2018-03-26 21:34:40 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<head>
+  <meta charset="UTF-8">
+</head>
+
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<title>Hello World~!~!</title>
+
+	</head>
+
+	<body>
+		<div>
+			ID :
+			<input type="text" value="" placeholder="아이디" style="display:block;"/>  <!--Input 태그의 속성-->
+			PASSWORD :
+			<input type="PASSWORD" value="" placeholder="비밀번호" style="display:block;"/>
+			<button>LOGIN</button>
+
+			<a href="/signup.php" style = "border-bottom : 1px solid blue; display:inline-block; font-size:8px;" >Register</a>
+		</div>
+	</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>Register</title>
+	</head>
+	<body>
+		<p>Hello! this page is signup page</p>
+	</body>
+</html>
